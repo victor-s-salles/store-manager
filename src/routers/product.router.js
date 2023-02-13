588df401ac446e81f979b48c07cfa4f9ad529209
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-02-13 18:42:32 -0300

    Req 4

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const { productController } = require('../controllers');
 
+const validateNewProductFields = require('../middlewares/validateNewProductFields');
+
 const router = express.Router();
 
 router.get('/', productController.listProducts);
 
 router.get('/:id', productController.listProductsById);
 
-router.post('/', productController.insertProduct);
+router.post('/', validateNewProductFields, productController.insertProduct);
 
 module.exports = router;
